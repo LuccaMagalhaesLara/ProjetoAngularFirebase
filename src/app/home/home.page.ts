c18b6448e1f4c85fea36678273910813444d2843
LuccaMagalhaesLara <162513029+LuccaMagalhaesLara@users.noreply.github.com>
2024-08-13 23:22:17 +0000

    Pending changes exported from your codespace

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -10,6 +10,25 @@ import { MessageService } from '../services/message.service';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  pessoa = {
+    foto: 'https://i.pinimg.com/736x/4e/f3/2d/4ef32dbfc1758cdf1a1c769231607a0c.jpg',
+    nome: 'Lucca Magalhães Lara',
+    ocupacao: 'Programador',
+    email: '@luccamagalhaes',
+    lingua: 'Português',
+    cep: 'Tatuí/SP',
+    atuacao: 'desde Janeiro de 2020'
+  },
+  postagens: [
 
+  ],
+  conta: [
+    {
+      num: '45.6K',
+      curtida: 'curtidas',
+      seguindo: 'seguindo',
+      amigos: 'amigos'
+    }
+  ]
   constructor(){ }
 }
